9d61d66399bda441ca7522225934d3643f1c16fd
tanimura <tanimura@tanimuranoMac-Studio.local>
2026-03-16 23:32:49 +0900

    feat: initial commit

---
+++ src/karma-tags.d.ts
@@ -0,0 +1,13 @@
+declare module 'react' {
+  interface HTMLAttributes<T> {
+    'data-karma-action'?: string
+    'data-karma-context'?: string
+    'data-karma-next'?: string
+    'data-karma-auth'?: 'required' | 'optional' | 'token' | 'none'
+    'data-karma-security'?: string
+    'data-karma-test-id'?: string
+    'data-karma-role'?: string
+    'data-karma-entity'?: string
+    'data-karma-state'?: string
+  }
+}
